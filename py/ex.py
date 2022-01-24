9a7e9c1437e426d9a2905286ffa58e60acaeef9e
afreedicp <youremail@domain.com>
2022-01-24 22:33:34 +0530

    my commit

---
+++ py/ex.py
@@ -0,0 +1,12 @@
+string= input("enter a string :")
+words=1
+sentnce=0
+for i in string:     
+     if(i==' ' or i==". "  or i== "? "):
+          words=words+1
+     if(i=="." or i== "?"):  
+          sentnce=sentnce+1
+
+# print("number of words :",words)    
+# print ("number of sentence :",sentnce)      
+
