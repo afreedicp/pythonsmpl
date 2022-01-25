bf7e1828fecf5cd907657bf3f99bfe42050321d0
Muhammed Hassan Afreedi <83634993+afreedicp@users.noreply.github.com>
2022-01-25 09:54:14 +0530

    Update ex.py

--- py/ex.py
+++ py/ex.py
@@ -1,12 +1,13 @@
-string= input("enter a string :")
-words=1
-sentnce=0
-for i in string:     
-     if(i==' ' or i==". "  or i== "? "):
-          words=words+1
-     if(i=="." or i== "?"):  
-          sentnce=sentnce+1
+def wrd():
+     string= input("enter a string :")
+     words=1
+     sentnce=0
+     for i in string:     
+          if(i==' ' or i==". "  or i== "? "):
+               words=words+1
+          if(i=="." or i== "?"):  
+               sentnce=sentnce+1
 
-# print("number of words :",words)    
-# print ("number of sentence :",sentnce)      
-
+     # print("number of words :",words)    
+     # print ("number of sentence :",sentnce)      
+wrd()
